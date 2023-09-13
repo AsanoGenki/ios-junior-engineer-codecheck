34ccdda4faafb58ac932f93f831b2600218b9693
AsanoGenki <asanogenki0218@icloud.com>
2023-09-13 10:06:52 +0900

    First commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+# ios-junior-engineer-codecheck
---
+++ ios-junior-engineer-codecheck/ios_junior_engineer_codecheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_junior_engineer_codecheckApp.swift
+//  ios-junior-engineer-codecheck
+//
+//  Created by Genki on 9/13/23.
+//
+
+import SwiftUI
+
+@main
+struct ios_junior_engineer_codecheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
