07c7010bd234f52655dce08511bbe28c31f59228
AsanoGenki <asanogenki0218@icloud.com>
2023-09-13 16:12:23 +0900

    fix: Result構造体の要素の記述を修正

--- ios-junior-engineer-codecheck/Views/Result/Result.swift
+++ ios-junior-engineer-codecheck/Views/Result/Result.swift
@@ -16,7 +16,7 @@ struct Result {
 
     var todofuken: String = "富山県"
     var capital: String = "富山市"
-    var citizanDay: MonthDay = MonthDay(month: 5, day: 9)
+    var citizanDay: MonthDay? = MonthDay(month: 5, day: 9)
     var hasCoastLine: Bool = true
     var logoURL: URL = URL(string: "https://japan-map.com/wp-content/uploads/toyama.png")!
     var brief: String = "富山県（とやまけん）は、日本の中部地方に位置する県。県庁所在地は富山市。\n中部地方の日本海側、新潟県を含めた場合の北陸地方のほぼ中央にある。\n※出典: フリー百科事典『ウィキペディア（Wikipedia）』"
