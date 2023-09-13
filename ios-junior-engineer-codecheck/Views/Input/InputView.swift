d610e7e8907396d8f28c9acaa1485d9d8771f7b9
AsanoGenki <asanogenki0218@icloud.com>
2023-09-13 14:55:06 +0900

    fix: TextFieldキーボード表示時の自動スクロールを制御

--- ios-junior-engineer-codecheck/Views/Input/InputView.swift
+++ ios-junior-engineer-codecheck/Views/Input/InputView.swift
@@ -14,93 +14,96 @@ struct InputView: View {
     @State var isShowingBloodTypeSheet = false
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 28) {
-            Text("あなたについて教えてください")
-                .font(.system(size: 24))
-                .fontWeight(.medium)
-                .padding(.top, 50)
-            VStack(alignment: .leading) {
-                Text("名前")
-                    .font(.title3)
-                TextField("山田太郎",
-                          text: $dataController.userName,
-                          onEditingChanged: { begin in
-                    if begin {
-                        self.editting = true
-                    } else {
-                        self.editting = false
+        GeometryReader { _ in
+            VStack(alignment: .leading, spacing: 28) {
+                Text("あなたについて教えてください")
+                    .font(.system(size: 24))
+                    .fontWeight(.medium)
+                    .padding(.top, 50)
+                VStack(alignment: .leading) {
+                    Text("名前")
+                        .font(.title3)
+                    TextField("山田太郎",
+                              text: $dataController.userName,
+                              onEditingChanged: { begin in
+                        if begin {
+                            self.editting = true
+                        } else {
+                            self.editting = false
+                        }
+                        
+                    })
+                    .padding(.all)
+                    .background{
+                        RoundedRectangle(cornerRadius: 12)
+                            .stroke(
+                                editting ? Color.green : Color(UIColor.separator),
+                                lineWidth: 2
+                            )
                     }
-                    
-                })
-                .padding(.all)
-                .background{
-                    RoundedRectangle(cornerRadius: 12)
-                        .stroke(
-                            editting ? Color.green : Color(UIColor.separator),
-                            lineWidth: 2
-                        )
                 }
-            }
-            
-            VStack(alignment: .leading) {
-                Text("誕生日")
-                    .font(.title3)
-                Text(dateToString(dateString: dataController.birthDay)!)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.all)
-                    .background{
-                        RoundedRectangle(cornerRadius: 12)
-                            .stroke(
-                                isShowingBirthdaySheet ? Color.green : Color(UIColor.separator),
-                                lineWidth: 2
-                            )
-                    }
-                    .contentShape(RoundedRectangle(cornerRadius: 12))
-                    .onTapGesture {
-                        self.isShowingBirthdaySheet.toggle()
-                    }
-            }
-            
-            VStack(alignment: .leading) {
-                Text("血液型")
-                    .font(.title3)
-                Text(dataController.bloodType)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.all)
-                    .background{
-                        RoundedRectangle(cornerRadius: 12)
-                            .stroke(
-                                isShowingBloodTypeSheet ? Color.green : Color(UIColor.separator),
-                                lineWidth: 2
-                            )
-                    }
-                    .contentShape(RoundedRectangle(cornerRadius: 12))
-                    .onTapGesture {
-                        self.isShowingBloodTypeSheet.toggle()
-                    }
                 
+                VStack(alignment: .leading) {
+                    Text("誕生日")
+                        .font(.title3)
+                    Text(dateToString(dateString: dataController.birthDay)!)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.all)
+                        .background{
+                            RoundedRectangle(cornerRadius: 12)
+                                .stroke(
+                                    isShowingBirthdaySheet ? Color.green : Color(UIColor.separator),
+                                    lineWidth: 2
+                                )
+                        }
+                        .contentShape(RoundedRectangle(cornerRadius: 12))
+                        .onTapGesture {
+                            self.isShowingBirthdaySheet.toggle()
+                        }
+                }
+                
+                VStack(alignment: .leading) {
+                    Text("血液型")
+                        .font(.title3)
+                    Text(dataController.bloodType)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.all)
+                        .background{
+                            RoundedRectangle(cornerRadius: 12)
+                                .stroke(
+                                    isShowingBloodTypeSheet ? Color.green : Color(UIColor.separator),
+                                    lineWidth: 2
+                                )
+                        }
+                        .contentShape(RoundedRectangle(cornerRadius: 12))
+                        .onTapGesture {
+                            self.isShowingBloodTypeSheet.toggle()
+                        }
+                    
+                }
+                Button {
+                    print("占うが押されました")
+                } label: {
+                    ButtonView(text: "占う", color: .blue)
+                }
+                .frame(maxWidth: .infinity, alignment: .center)
+                
+                Spacer()
             }
-            Button {
-                print("占うが押されました")
-            } label: {
-                ButtonView(text: "占う", color: .blue)
+            .sheet(isPresented: $isShowingBirthdaySheet) {
+                birthdaySheet(birthDay: $dataController.birthDay)
+                    .presentationDetents([ .fraction(0.35)])
             }
-            .frame(maxWidth: .infinity, alignment: .center)
+            .sheet(isPresented: $isShowingBloodTypeSheet) {
+                bloodTypeSheet(bloodType: $dataController.bloodType)
+                    .presentationDetents([ .fraction(0.35)])
+            }
             
-            Spacer()
+            .padding(.leading)
+            .padding(.trailing)
+            
         }
-        .sheet(isPresented: $isShowingBirthdaySheet) {
-            birthdaySheet(birthDay: $dataController.birthDay)
-                .presentationDetents([ .fraction(0.35)])
-                }
-        .sheet(isPresented: $isShowingBloodTypeSheet) {
-            bloodTypeSheet(bloodType: $dataController.bloodType)
-                .presentationDetents([ .fraction(0.35)])
-                }
-        
-        .padding(.leading)
-        .padding(.trailing)
-        
+        .ignoresSafeArea(.keyboard, edges: .bottom)
     }
     
     func dateToString(dateString: Date) -> String? {
