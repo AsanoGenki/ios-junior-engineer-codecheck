60e4e088d4b9b003b7259ea632be387a5b2c4275
AsanoGenki <asanogenki0218@icloud.com>
2023-09-13 13:04:07 +0900

    add: 血液型を入力するシートの作成

---
+++ ios-junior-engineer-codecheck/Input/bloodTypeSheet.swift
@@ -0,0 +1,35 @@
+//
+//  bloodTypeSheet.swift
+//  ios-junior-engineer-codecheck
+//
+//  Created by Genki on 9/13/23.
+//
+
+import SwiftUI
+
+struct bloodTypeSheet: View {
+    @State var bloodType = "A型"
+    @Environment(\.dismiss) private var dismiss
+    var body: some View {
+        NavigationView {
+            Picker(selection: $bloodType, label: Text("血液型")) {
+                Text("A型").tag("A型")
+                Text("B型").tag("B型")
+                Text("AB型").tag("AB型")
+                Text("O型").tag("O型")
+            }
+            .pickerStyle(WheelPickerStyle())
+            .navigationBarItems(
+                trailing: Button("OK") {
+                    dismiss()
+                }
+            )
+        }
+    }
+}
+
+struct bloodTypeSheet_Previews: PreviewProvider {
+    static var previews: some View {
+        bloodTypeSheet()
+    }
+}
