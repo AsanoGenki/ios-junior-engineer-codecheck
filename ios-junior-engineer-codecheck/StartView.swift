72ecfde14e66fa0eca0cee6a3c92df9d4184a285
AsanoGenki <asanogenki0218@icloud.com>
2023-09-13 10:40:59 +0900

    add: スタート画面のベースUIの実装

---
+++ ios-junior-engineer-codecheck/StartView.swift
@@ -0,0 +1,46 @@
+//
+//  StartView.swift
+//  ios-junior-engineer-codecheck
+//
+//  Created by Genki on 9/13/23.
+//
+
+import SwiftUI
+
+struct StartView: View {
+    var body: some View {
+        VStack {
+            Text("都道府県")
+                .font(.system(size: 54))
+                .fontWeight(.semibold)
+                .foregroundColor(.green)
+            Text("相性占い")
+                .font(.system(size: 54))
+                .fontWeight(.semibold)
+                .foregroundColor(.green)
+            
+            Image("nihonchizu")
+                .resizable()
+                .scaledToFit()
+                .frame(width: UIScreen.main.bounds.width * 0.8)
+                .padding(.top, 20)
+            
+            NavigationLink {
+                
+            } label: {
+                Text("スタート")
+                    .frame(minWidth: 150)
+                    .foregroundColor(.white)
+                    .font(.system(.title2, design: .rounded).bold())
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 15).fill(Color.blue.opacity(0.8)))
+            }
+        }
+    }
+}
+
+struct StartView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartView()
+    }
+}
--- ios-junior-engineer-codecheck/ios_junior_engineer_codecheckApp.swift
+++ ios-junior-engineer-codecheck/ios_junior_engineer_codecheckApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ios_junior_engineer_codecheckApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            StartView()
         }
     }
 }
