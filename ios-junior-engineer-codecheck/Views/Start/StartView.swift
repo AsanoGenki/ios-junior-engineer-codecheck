c40cdb9760c487d737d3beb788a3fd4dd329bb55
AsanoGenki <asanogenki0218@icloud.com>
2023-09-17 11:14:13 +0900

    change: スタート画面のUIを変更

--- ios-junior-engineer-codecheck/Views/Start/StartView.swift
+++ ios-junior-engineer-codecheck/Views/Start/StartView.swift
@@ -15,67 +15,72 @@ struct StartView: View {
     
     var body: some View {
         NavigationStack {
-            VStack {
-                VStack(spacing: 20) {
-                    Text("都道府県")
-                        .font(.custom("Corporate-Logo-Rounded-Bold-ver3", size: 64))
-                        .fontWeight(.semibold)
-                        .foregroundColor(.green)
-                    Text("相性占い")
-                        .font(.custom("Corporate-Logo-Rounded-Bold-ver3", size: 64))
-                        .fontWeight(.semibold)
-                        .foregroundColor(.green)
-                    Text("〜今日相性が良い都道府県を占おう！〜")
-                        .font(.custom("Corporate-Logo-Rounded-Bold-ver3", size: 18))
-                        .fontWeight(.semibold)
-                        .foregroundColor(.green)
-                }
-                
-                
-                Image("nihonchizu")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: UIScreen.main.bounds.width * 0.8)
-                    .padding(.top, 20)
-                
-                Button {
-                    dataController.playClickNormal()
-                    isShowingInputView.toggle()
-                } label: {
-                    ButtonView(text: "はじめる", color: .blue)
-                }
-                
-                .navigationDestination(isPresented: $isShowingInputView, destination: {
-                    InputView()
-                })
-                .navigationDestination(isPresented: $isShowingRecordView, destination: {
-                    RecordView()
-                    
-                })
-                
-                .sheet(isPresented: $isShowingSettingView) {
-                    SettingView()
-                        .presentationDetents([ .fraction(0.4)])
-                }
-                
-                .navigationBarItems(
-                    
-                    leading: Button {
-                        dataController.playClickSmall()
-                        isShowingSettingView.toggle()
-                    } label: {
-                        Image(systemName: "gearshape")
-                            .font(.system(size: 18))
-                    },
-                    
-                    trailing: Button {
-                        dataController.playClickSmall()
-                        isShowingRecordView.toggle()
-                    } label: {
-                        Image(systemName: "timer")
-                            .font(.system(size: 18))
+            ZStack {
+                Color("backgroundColor")
+                    .ignoresSafeArea()
+                VStack {
+                    VStack(spacing: 20) {
+                        Text("都道府県")
+                            .font(.custom("Corporate-Logo-Rounded-Bold-ver3", size: 64))
+
+                        Text("相性占い")
+                            .font(.custom("Corporate-Logo-Rounded-Bold-ver3", size: 64))
+
+                        Text("〜今日相性が良い都道府県を占おう！〜")
+                            .font(.custom("Corporate-Logo-Rounded-Bold-ver3", size: 18))
+
                     }
-                )
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    
+                    
+                    Image("nihonchizu")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: UIScreen.main.bounds.width * 0.8)
+                        .padding(.top, 20)
+                    
+                    Button {
+                        dataController.playClickNormal()
+                        isShowingInputView.toggle()
+                    } label: {
+                        ButtonView(text: "はじめる", color: .blue)
+                    }
+                    
+                    .navigationDestination(isPresented: $isShowingInputView, destination: {
+                        InputView()
+                    })
+                    .navigationDestination(isPresented: $isShowingRecordView, destination: {
+                        RecordView()
+                        
+                    })
+                    
+                    .sheet(isPresented: $isShowingSettingView) {
+                        SettingView()
+                            .presentationDetents([ .fraction(0.4)])
+                    }
+                    
+                    .navigationBarItems(
+                        
+                        leading: Button {
+                            dataController.playClickSmall()
+                            isShowingSettingView.toggle()
+                        } label: {
+                            Image(systemName: "gearshape")
+                                .font(.system(size: 24))
+                                .foregroundColor(.white)
+                        },
+                        
+                        trailing: Button {
+                            dataController.playClickSmall()
+                            isShowingRecordView.toggle()
+                        } label: {
+                            Image(systemName: "timer")
+                                .font(.system(size: 24))
+                                .foregroundColor(.white)
+                        }
+                    )
+                }
             }
         }
     }
